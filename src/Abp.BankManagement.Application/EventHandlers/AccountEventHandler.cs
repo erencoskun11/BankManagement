5b9c9074e3d48cac794ad6c9789a66d791fffdb2
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-06 13:23:06 +0300

    Event Handlers updated accordingly.

---
+++ src/Abp.BankManagement.Application/EventHandlers/AccountEventHandler.cs
@@ -0,0 +1,44 @@
+﻿/*using Abp.BankManagement.Etos.AccountEtos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.EventBus.Distributed;
+
+namespace Abp.BankManagement.EventHandlers
+{
+    public class AccountEventHandler : 
+        IDistributedEventHandler<AccountCreatedEto>,
+        IDistributedEventHandler<AccountUpdatedEto>,
+        IDistributedEventHandler<AccountEto>
+    {
+        private readonly ILogger<AccountEventHandler> _logger;
+
+        public AccountEventHandler(ILogger<AccountEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleEventAsync(AccountCreatedEto eventData)
+        {
+            _logger.LogInformation($"[Account Created] Name : {eventData.AccountName}, IBAN: {eventData.IBAN}");
+            return Task.CompletedTask;
+        }
+
+        public Task HandleEventAsync(AccountUpdatedEto eventData)
+        {
+            _logger.LogInformation($"[Account Updated] Name: {eventData.AccountName}, Active: {eventData.IsActive}");
+            return Task.CompletedTask;
+
+        }
+
+        Task IDistributedEventHandler<AccountEto>.HandleEventAsync(AccountEto eventData)
+        {
+            _logger.LogInformation($"[Account Listed] Name: {eventData.AccountName}, Number: {eventData.AccountNumber}");
+            return Task.CompletedTask;
+        }
+    }
+}
+*/
---
+++ src/Abp.BankManagement.Application/EventHandlers/CustomerCreateEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using Abp.BankManagement.Etos.CustomerEtos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.EventBus.Distributed;
+
+namespace Abp.BankManagement.EventHandlers
+{
+    public class CustomerCreateEventHandler : IDistributedEventHandler<CustomerCreateEto>
+    {
+        private readonly ILogger _logger;
+
+        public CustomerCreateEventHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleEventAsync(CustomerCreateEto eventData)
+        {
+            _logger.LogInformation($"[Customer Created] Name: {eventData.FullName}, NationalId: {eventData.NationalId}");
+            return Task.CompletedTask;
+        }
+    }
+}
